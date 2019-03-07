f7c730f108394db60c7039bf7fefbc41a1ad4122
NyanKiyoshi <hello@vanille.bid>
2019-03-07 11:31:32 +0100

    Remove service worker

--- tp2/src/index.tsx
+++ tp2/src/index.tsx
@@ -2,11 +2,5 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import ItemListApp from './ItemListApp';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<ItemListApp />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
