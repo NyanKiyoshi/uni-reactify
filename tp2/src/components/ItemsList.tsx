34cd161302a4d5617c55a109851213456018689c
NyanKiyoshi <hello@vanille.bid>
2019-03-07 11:47:07 +0100

    Simplify stateless components

--- tp2/src/components/ItemsList.tsx
+++ tp2/src/components/ItemsList.tsx
@@ -1,29 +1,20 @@
 import * as React from 'react';
-import {Component} from "react";
 import ListItem from "./ListItem";
 
-export default class ItemsList
-        extends Component<IItemsListProps, never> {
-
-    public constructor(props: IItemsListProps) {
-        super(props);
-    }
-
-    public render(): any {
-        return (
-            <div className="ItemsList">
-                {
-                    this.props.items.map((value, index) => {
-                        return (
-                            <ListItem
-                                text={value}
-                                index={index}
-                                key={index}
-                                onRemove={this.props.onRemoveItem} />
-                        );
-                    })
-                }
-            </div>
-        );
-    }
-}
+export default (props: IItemsListProps) => {
+    return (
+        <div className="ItemsList">
+            {
+                props.items.map((value, index) => {
+                    return (
+                        <ListItem
+                            text={value}
+                            index={index}
+                            key={index}
+                            onRemove={props.onRemoveItem} />
+                    );
+                })
+            }
+        </div>
+    );
+};
--- tp2/src/components/ListItem.tsx
+++ tp2/src/components/ListItem.tsx
@@ -1,21 +1,14 @@
 import * as React from 'react';
 import {Component} from 'react';
 
-export default class ListItem
-        extends Component<IListItemProps, never> {
-
-    public constructor(props: IListItemProps) {
-        super(props);
-    }
-
-    public render(): any {
-        return (
-            <div className="ListItem">
-                <p className="text">{this.props.text}</p>
-                <button type="button" onClick={() => this.props.onRemove(this.props.index)}>
-                    Delete
-                </button>
-            </div>
-        );
-    }
-}
+export default (props: IListItemProps) => {
+    return (
+        <div className="ListItem">
+            <p className="text">{props.text}</p>
+            <button type="button"
+                    onClick={() => props.onRemove(props.index)}>
+                Delete
+            </button>
+        </div>
+    );
+};
