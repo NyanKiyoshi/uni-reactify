e40379af97c33dd25efb685d231d748fdc4d9a48
NyanKiyoshi <hello@vanille.bid>
2019-03-07 08:47:44 +0100

    Initial project

---
+++ tp1/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Clock from './components/Clock';
+import './App.css';
+
+class App extends Component {
+  public render() : any {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <Clock />
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
